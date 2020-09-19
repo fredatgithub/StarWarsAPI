eb91a3f3be7e2436932aabc43f9306b2108e8d17
Fred <domaine75@gmail.com>
2020-09-20 00:56:03 +0200

    Add project to get API JSON files

---
+++ ConsoleAppGetAPI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppGetAPI
+{
+  class Program
+  {
+    static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display("Get JSON from Starwars API");
+      string url1 = "https://swapi.dev/api/people/1/";
+
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
